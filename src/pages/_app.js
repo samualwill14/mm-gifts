34d24fadbe0986573d2877a5ee58b80d4cdf5590
samualwill14 <tikmoneycalculator@gmail.com>
2026-04-07 13:19:07 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
+import Head from 'next/head'; // 1. Head ko import karein
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Ye tag ab aapke saare pages (Home, About, Privacy etc.) par dikhega */}
+        <meta name="google-site-verification" content="hEGXcxltxQUp9y4oYGnaIR1QsAAqkhU0EIV4gOwZKkk" />
+      </Head>
+      
+      <Component {...pageProps} />
+    </>
+  );
 }
