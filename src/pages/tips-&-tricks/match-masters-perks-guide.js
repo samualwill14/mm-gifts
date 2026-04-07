eb2f4af025edbc417bdd32fd01352e34390766cf
samualwill14 <tikmoneycalculator@gmail.com>
2026-04-07 13:15:19 +0530

    Update match-masters-perks-guide.js

--- src/pages/tips-&-tricks/match-masters-perks-guide.js
+++ src/pages/tips-&-tricks/match-masters-perks-guide.js
@@ -43,6 +43,13 @@ export default function MatchMastersPerksGuide() {
         <meta property="og:url" content="https://mmfreegifts.de/tips-&-tricks/match-masters-perks-guide/" />
         <meta property="og:type" content="article" />
         <meta property="og:site_name" content="mmfreegifts" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="SGH Free Coins" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <NotificationBar
