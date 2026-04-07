222bfafaaa6f04ec45b851efb6152b4f7f3e6202
samualwill14 <tikmoneycalculator@gmail.com>
2026-04-08 04:59:35 +0530

    Update get-free-stickers-match-masters.js

--- src/pages/tips-&-tricks/get-free-stickers-match-masters.js
+++ src/pages/tips-&-tricks/get-free-stickers-match-masters.js
@@ -50,7 +50,7 @@ export default function FreeStickersMM() {
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-title" content="SGH Free Coins" />
+        <meta name="apple-mobile-web-app-title" content="MM Free Gifts" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
