13b761921043d553c624594aa23082cec4c2bff9
samualwill14 <tikmoneycalculator@gmail.com>
2026-04-09 20:38:54 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,34 @@
-import Head from 'next/head'; // 1. Head ko import karein
+import Head from 'next/head';
+import Script from 'next/script';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Ye tag ab aapke saare pages (Home, About, Privacy etc.) par dikhega */}
-        <meta name="google-site-verification" content="hEGXcxltxQUp9y4oYGnaIR1QsAAqkhU0EIV4gOwZKkk" />
+        {/* Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="hEGXcxltxQUp9y4oYGnaIR1QsAAqkhU0EIV4gOwZKkk"
+        />
       </Head>
-      
+
+      {/* Google Analytics Script */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-288PZPRDVH"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-288PZPRDVH', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </>
   );
